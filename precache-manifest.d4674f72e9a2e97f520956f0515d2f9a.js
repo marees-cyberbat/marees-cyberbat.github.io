cb63274b25233d8a47aabd59c3056b1fdd6b0434
findmarees <mareeskratos@gmail.com>
2020-05-30 18:12:52 +0530

    Updates

---
+++ precache-manifest.d4674f72e9a2e97f520956f0515d2f9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecad4a8fda721f43ed12db52340c14c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e41796040185ff2eb0bf",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "844885f6f789fe3878d8",
+    "url": "/static/css/main.1bc9eba2.chunk.css"
+  },
+  {
+    "revision": "e41796040185ff2eb0bf",
+    "url": "/static/js/2.1bebd9bc.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/static/js/2.1bebd9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844885f6f789fe3878d8",
+    "url": "/static/js/main.5d51497c.chunk.js"
+  },
+  {
+    "revision": "97afc6ebaf4d6a4d96b1",
+    "url": "/static/js/runtime-main.7ffd5d06.js"
+  }
+]);
